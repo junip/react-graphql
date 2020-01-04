b207c505fa66f7df8b06ac688e3798758812a069
Junip Dewan <junipd2@gmail.com>
2020-01-04 22:35:46 +0530

    added another authortype query in the field

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const express =  require('express')
-const graphqlHTTP = require('express-graphql')
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
 const app = express();
-const schema = require('./schema/schema')
+const schema = require("./schema/schema");
 
 // mount the express graphql as route handler
 // use get or post to show it in the graphiql in the browser
@@ -9,11 +9,5 @@ const schema = require('./schema/schema')
  * without the schema in the graphiql browser it will show error that the graphql middle ware
  * needs the graphql middleware.
  */
-app.use(
-  '/graphql',
-  graphqlHTTP({
-    schema,
-    graphiql: true,
-  }),
-);
-app.listen(4000); 
+app.use("/graphql", graphqlHTTP({ schema, graphiql: true }));
+app.listen(4000);
--- schema/schema.js
+++ schema/schema.js
@@ -1,41 +1,81 @@
-const graphql = require('graphql');
+const graphql = require("graphql");
 //const _ = require('loadash');
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLID,
+  GraphQLInt
+} = graphql;
 
 // defining the book object with the expected fieids and its type
 
 // for the instacne defining consts
 
 const books = [
-    {name: 'the name of the wind', genre: 'Fantasy', id: "1"},
-    {name: 'The long earth', genre: 'Fantasy', id: "2"},
-    {name: 'Rich dad and poor dad', genre: 'Sci-Fi', id: "3"}
-]
+  { name: "the name of the wind", genre: "Fantasy", id: "1" },
+  { name: "The long earth", genre: "Fantasy", id: "2" },
+  { name: "Rich dad and poor dad", genre: "Sci-Fi", id: "3" }
+];
+
+const authors = [
+  {
+    name: "Patrik",
+    age: 22,
+    id: 1
+  },
+  {
+    name: "Brandan",
+    age: 24,
+    id: 2
+  },
+  {
+    name: "Tony",
+    age: 26,
+    id: 3
+  }
+];
 
 const BookType = new GraphQLObjectType({
-    name: 'Book',
-    fields: ()=> ({
-        id: {type: GraphQLString },
-        name: {type: GraphQLString },
-        genre: {type: GraphQLString } 
-    })
-})
+  name: "Book",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    genre: { type: GraphQLString }
+  })
+});
+
+const AuthorType = new GraphQLObjectType({
+  name: "Author",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt }
+  })
+});
 
 const RootQuery = new GraphQLObjectType({
-    name: 'RootQueryType',
-    fields: {
-        book: {
-            type: BookType,
-            args: { id: { type: GraphQLString }},
-            resolve(parent, args){
-                // code to get the data from the database or other resoureces
-               return  books[0]            
-            }
-        }
+  name: "RootQueryType",
+  fields: {
+    book: {
+      type: BookType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        // code to get the data from the database or other resoureces
+        return books[0];
+      }
+    },
+    author: {
+      type: AuthorType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return authors[0];
+      }
     }
-})
+  }
+});
 
 module.exports = new GraphQLSchema({
-    query: RootQuery
-})
+  query: RootQuery
+});
